297da1a343b732e6e5a9128699d2ab0121350442
Shoon-Laat <shoonlaatpyae@gmail.com>
2024-10-10 10:35:27 +0630

    'banner width fix'

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -5,8 +5,8 @@ export default function Banner({id}){
 return(
     <div className="banner md:h-screen bg-custom-image bg-cover bg-center relative">
     <ScrollAnimation  animateIn='fadeIn'>
-        <div id={id} className="container mx-auto max-w-mobile md:max-w-pc flex flex-wrap flex-col-reverse md:flex-row justify-between items-center md:pt-32 pb-0 py-20 md:py-0">
-        <div className="xl:w-2/5 md:w-2/4 font-bold w-full">
+        <div id={id} className="container mx-auto max-w-mobile md:max-w-pc flex flex-wrap flex-col-reverse md:flex-row justify-between items-center md:py-32 py-20">
+        <div className="xl:w-3/5 md:w-2/4 font-bold w-full">
         <ScrollAnimation animateIn='bounceInLeft' duration={3}>
           <span className="custom-text text-sm md:text-base xl:text-lg">Hello! I'm</span>
           <h2 className="xl:text-5xl md:text-3xl custom-text pt-2">Shoon Laat Pyae
@@ -15,7 +15,7 @@ return(
           <p className="custom-text pt-2 text-sm md:text-base xl:text-lg">I am a dedicated Frontend Developer with expertise in React and Next.js, specializing in creating visually engaging, responsive web applications. With a strong command of HTML, CSS, and JavaScript, I transform design concepts into seamless, high-performing user interfaces across all devices.</p>
         </ScrollAnimation>
         </div>
-        <div className='md:w-2/4 w-full'>
+        <div className='md:w-4/12 w-full'>
         <ScrollAnimation animateIn='bounceInRight' duration={5}>
         <Image className='mx-auto2 img-bounce w-full h-full'
         src="/img/mv.png"
